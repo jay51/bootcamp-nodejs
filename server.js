a4c80a1e519a1a31e3681f80269aba90abd2cc4a
jay51 <nenojaafar3@gmail.com>
2020-03-13 21:10:36 -0500

    Express Router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-
 const express = require("express");
 const morgan = require("morgan");
+
+const campsiteRouter = require("./routes/campsiteRouter");
 
 
 const HOSTNAME = "localhost";
@@ -11,48 +12,25 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
+app.use("/campsites", campsiteRouter);
 
-app.all('/campsites', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
-
-// curl -X POST -v -H "Content-Type: application/json" -d '{"name":"value1", "description":"value2"}' http://localhost:3000/campsites/
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
-
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
-});
-
-app.get('/campsites/:campsiteId', (req, res) => {
+app.get("/campsites/:campsiteId", (req, res) => {
     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
 });
 
-app.post('/campsites/:campsiteId', (req, res) => {
+app.post("/campsites/:campsiteId", (req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
 });
 
-// curl -X PUT -v -H "Content-Type: application/json" -d '{"name":"value1", "description":"value2"}' http://localhost:3000/campsites/23
-app.put('/campsites/:campsiteId', (req, res) => {
+// curl -X PUT -v -H "Content-Type: application/json" -d "{"name":"value1", "description":"value2"}" http://localhost:3000/campsites/23
+app.put("/campsites/:campsiteId", (req, res) => {
     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
     res.end(`Will update the campsite: ${req.body.name}
     with description: ${req.body.description}`);
 });
 
-app.delete('/campsites/:campsiteId', (req, res) => {
+app.delete("/campsites/:campsiteId", (req, res) => {
     res.end(`Deleting campsite: ${req.params.campsiteId}`);
 });
 
