4f541e54803cb26ca3013b763465d241453549f0
jay51 <nenojaafar3@gmail.com>
2020-03-27 01:39:32 -0500

    cookies

--- app.js
+++ app.js
@@ -30,25 +30,40 @@ var app = express();
 
 
 function auth(req, res, next){
-
     console.log(req.headers)
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        const err = new Error("You are not authenticated!");
-        res.setHeader("WWW-Authenticate", "Basic");
-        err.status = 401;
-        return next(err);
-    }
-    
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-    const user = auth[0]
-    const pass = auth[1]
-    if(user == "admin" && pass == "password"){
-        return next(); //authorized
 
-    } else{
-        const err = new Error("You are not authenticated!");
-        res.setHeader("WWW-Authenticate", "Basic");
+    // check for signed cookie (if no cookie check for auth header)
+    if(!req.signedCookies.user){
+        const authHeader = req.headers.authorization;
+        if(!authHeader){
+            const err = new Error("You are not authenticated!");
+            res.setHeader("WWW-Authenticate", "Basic");
+            err.status = 401;
+            return next(err);
+        }
+
+
+        const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+        const user = auth[0]
+        const pass = auth[1]
+        if(user === "admin" && pass === "password"){
+             res.cookie("user", "admin", {signed: true});
+            return next(); //authorized
+
+        } else{
+            const err = new Error("You are not authenticated!");
+            res.setHeader("WWW-Authenticate", "Basic");
+            err.status = 401;
+            return next(err);
+        }
+
+    } else {
+        console.log("ok you have a cookie");
+        if(req.signedCookies.user === "admin"){
+            return next();
+        }
+
+        const err = Error("You are not authenticated!");
         err.status = 401;
         return next(err);
     }
@@ -63,7 +78,7 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("12345-67890-09876-54321"));
 app.use(auth)
 app.use(express.static(path.join(__dirname, "public")));
 
